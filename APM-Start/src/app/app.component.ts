e9e86d52286b10fc2fff6b8979b9ff576d40049a
AnthonyRMatter <134010366+AnthonyRMatter@users.noreply.github.com>
2023-09-05 20:07:46 +0000

    third

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'pm-root', //pm was created through ng new (NAME) --prefix pm
   template: `
-  <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
+  <div><h1>{{pageTitle}}</h1>  
+      <pm-products></pm-products>
   </div>
-  `
+  ` // Information within {{ }} is called a Template Expression
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent // Tells Angular that this componenet is now a part of the application's modules
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule
+  ], // Modules that are external to our Angular application
   bootstrap: [AppComponent] // Starting component for application
 })
 export class AppModule { }
