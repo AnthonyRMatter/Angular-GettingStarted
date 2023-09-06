da91a3366e95799afbf00ff955831041473e0420
AnthonyRMatter <134010366+AnthonyRMatter@users.noreply.github.com>
2023-09-06 01:12:16 +0000

    fifth

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent // Tells Angular that this componenet is now a part of the application's modules
+    WelcomeComponent,
+    ProductListComponent, // Tells Angular that this componenet is now a part of the application's modules
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id', component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+
+    ])
   ], // Modules that are external to our Angular application
   bootstrap: [AppComponent] // Starting component for application
 })
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 // @Component({
 //     selector: 'pm-products',
@@ -6,42 +9,64 @@ import { Component } from "@angular/core";
 // })
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
-        {
-            "productID": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden-cart.png"
-        },
-        {
-            "productID": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+    errorMessage: string = '';
+    sub!: Subscription;
+    // listFilters: string = 'cart';
+
+    private _listFilter: string = '';
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string) {
+        this._listFilter = value;
+        console.log('In setter:', value);
+        this.filteredProducts = this.performFilter(value);
+    }
+
+
+    filteredProducts: IProduct[] = [];
+    product: IProduct[] = [];
+
+    constructor(private productService: ProductService) {
+        
+    }
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
+
+    performFilter(filterBy: string) : IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.product.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().includes(filterBy));
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts().subscribe({
+            next: product => {
+                this.product = product;
+                this.filteredProducts = this.product;
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 
 }
